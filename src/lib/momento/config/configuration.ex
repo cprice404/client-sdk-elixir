4edd17fb05c1507396927f0f81c80ea9c8dca4f3
Chris Price <chris@momentohq.com>
2023-06-08 21:45:16 -0700

    WIP

---
+++ src/lib/momento/config/configuration.ex
@@ -0,0 +1,10 @@
+defmodule Momento.Config do
+  defmodule Configuration do
+    @moduledoc """
+    Documentation for `Momento.Config.Configuration`.
+    """
+    defstruct []
+
+    @opaque t() :: %{}
+  end
+end
